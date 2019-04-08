9a849c8755290fede675bf637fb9fb48db8dcce6
yuhansun <wdmzssyh@gmail.com>
2019-04-08 18:25:54 +0800

    finish the middle of risotree connector

--- src/main/java/edu/asu/mywebapp/Start.java
+++ src/main/java/edu/asu/mywebapp/Start.java
@@ -1,73 +1,66 @@
 package edu.asu.mywebapp;
 
-import java.io.IOException;
-
 import org.glassfish.grizzly.http.server.HttpServer;
 import org.glassfish.grizzly.http.server.NetworkListener;
 import org.glassfish.grizzly.servlet.ServletRegistration;
 import org.glassfish.grizzly.servlet.WebappContext;
 import org.glassfish.jersey.servlet.ServletContainer;
+import commons.Util;
+import edu.asu.mywebapp.resources.TestResource;
 
 /**
- * The sample demonstrates how to make Jersey2-Spring integration work on top of
- * Grizzly 2, including static pages served from a folder or from within a jar
- * file.
+ * The sample demonstrates how to make Jersey2-Spring integration work on top of Grizzly 2,
+ * including static pages served from a folder or from within a jar file.
  * 
  * @author Janus Dam Nielsen
  */
 public class Start {
 
-	public static void main(String[] args) throws IOException {
-		startEmbeddedServer();
-	}
+  public static void main(String[] args) throws Exception {
+    startEmbeddedServer();
+  }
 
-	static void startEmbeddedServer() throws IOException {
+  static void startEmbeddedServer() throws Exception {
 
-		// Create test web application context.
-		WebappContext webappContext = new WebappContext("Test Context");
-		webappContext
-		.addContextInitParameter("contextClass",
-				"org.springframework.web.context.support.XmlWebApplicationContext");
-		webappContext.addContextInitParameter("contextConfigLocation",
-				"classpath*:spring-context.xml");
-		webappContext
-		.addListener("org.springframework.web.context.ContextLoaderListener");
+    // Create test web application context.
+    WebappContext webappContext = new WebappContext("Test Context");
+    webappContext.addContextInitParameter("contextClass",
+        "org.springframework.web.context.support.XmlWebApplicationContext");
+    webappContext.addContextInitParameter("contextConfigLocation", "classpath*:spring-context.xml");
+    webappContext.addListener("org.springframework.web.context.ContextLoaderListener");
 
-		// Create a servlet registration for the web application in order to wire up Spring managed collaborators to Jersey resources.
-		ServletRegistration servletRegistration = webappContext.addServlet(
-				"jersey-servlet", ServletContainer.class);
+    // Create a servlet registration for the web application in order to wire up Spring managed
+    // collaborators to Jersey resources.
+    ServletRegistration servletRegistration =
+        webappContext.addServlet("jersey-servlet", ServletContainer.class);
 
-		// The logging filters for server logging.
-		servletRegistration
-		.setInitParameter(
-				"com.sun.jersey.spi.container.ContainerResponseFilters",
-				"com.sun.jersey.api.container.filter.LoggingFilter");
-		servletRegistration.setInitParameter(
-				"com.sun.jersey.spi.container.ContainerRequestFilters",
-				"com.sun.jersey.api.container.filter.LoggingFilter");
+    // The logging filters for server logging.
+    servletRegistration.setInitParameter("com.sun.jersey.spi.container.ContainerResponseFilters",
+        "com.sun.jersey.api.container.filter.LoggingFilter");
+    servletRegistration.setInitParameter("com.sun.jersey.spi.container.ContainerRequestFilters",
+        "com.sun.jersey.api.container.filter.LoggingFilter");
 
-		servletRegistration.setInitParameter("javax.ws.rs.Application",
-				MywebappResourceRegistrator.class.getName());
+    servletRegistration.setInitParameter("javax.ws.rs.Application",
+        MywebappResourceRegistrator.class.getName());
 
-		servletRegistration.setInitParameter(
-				"com.sun.jersey.config.property.packages", "com.test");
-		servletRegistration.setInitParameter(
-				"com.sun.jersey.api.json.POJOMappingFeature", "true");
-		servletRegistration.addMapping("/*");
+    servletRegistration.setInitParameter("com.sun.jersey.config.property.packages", "com.test");
+    servletRegistration.setInitParameter("com.sun.jersey.api.json.POJOMappingFeature", "true");
+    servletRegistration.addMapping("/*");
 
-		HttpServer server = new HttpServer();
-		NetworkListener listener = new NetworkListener("grizzly2", "localhost",
-				3388);
-		server.addListener(listener);
+    HttpServer server = new HttpServer();
+    NetworkListener listener = new NetworkListener("grizzly2", "localhost", 3388);
+    server.addListener(listener);
 
-		webappContext.deploy(server);
+    webappContext.deploy(server);
 
-		try {
-			server.start();
-			System.out.println("Press enter to stop the server...");
-			System.in.read();
-		} finally {
-			server.shutdownNow();
-		}
-	}
+    try {
+      server.start();
+      System.out.println("Press enter to stop the server...");
+      System.in.read();
+      Util.close(TestResource.dbserviceRisoTree);
+      // TestResource.driver.close();
+    } finally {
+      server.shutdownNow();
+    }
+  }
 }
--- src/main/java/edu/asu/mywebapp/resources/TestResource.java
+++ src/main/java/edu/asu/mywebapp/resources/TestResource.java
@@ -1,17 +1,18 @@
 package edu.asu.mywebapp.resources;
 
 import java.security.SecureRandom;
+import java.util.List;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import org.neo4j.driver.v1.AuthTokens;
-import org.neo4j.driver.v1.Driver;
-import org.neo4j.driver.v1.GraphDatabase;
-import org.neo4j.driver.v1.Session;
-import org.neo4j.driver.v1.StatementResult;
+import org.apache.commons.lang3.StringUtils;
+import org.neo4j.graphdb.GraphDatabaseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import CypherMiddleWare.RisoTreeConnector;
+import commons.Config;
+import commons.Neo4jGraphUtility;
 import edu.asu.mywebapp.domain.User;
 import edu.asu.mywebapp.domain.interfaces.UserManager;
 
@@ -24,31 +25,60 @@ import edu.asu.mywebapp.domain.interfaces.UserManager;
 @Path("/")
 public class TestResource {
 
-  // public static GraphDatabaseService service = null;
+  final static String dataset = Config.Datasets.wikidata.name();
 
-  // public static void main() {
-  //
-  // }
+  // public Driver driver =
+  // GraphDatabase.driver("bolt://localhost:7687", AuthTokens.basic("neo4j", "syh19910205"));
+
+  static String risoTreeDbPath = "D:/Project_Data/wikidata-20180308-truthy-BETA.nt/"
+      + "neo4j-community-3.4.12/data/databases/graph.db";
+  public static GraphDatabaseService dbserviceRisoTree =
+      Neo4jGraphUtility.getDatabaseService(risoTreeDbPath);
+
+  // static String RTreeDbPath = "D:/Project_Data/wikidata-20180308-truthy-BETA.nt/"
+  // + "neo4j-community-3.4.12/data/databases/graph.db";
+  // public static GraphDatabaseService dbserviceRTree =
+  // Neo4jGraphUtility.getDatabaseService(RTreeDbPath);
 
   @Autowired
   private UserManager userManager;
 
   @GET
-  @Path("/cypherquery/{username}")
+  @Path("/risotree/{input}")
   @Produces("text/html")
-  public String cypherQuery(@PathParam("username") String query) {
-    System.out.println(query);
-    Driver driver =
-        GraphDatabase.driver("bolt://localhost:7687", AuthTokens.basic("neo4j", "syh19910205"));
-    String result = "";
-    Session session = driver.session();
-    StatementResult rs = session.run(query);
-    while (rs.hasNext()) {
-      result += rs.next();
+  public String risotree(@PathParam("input") String input) throws Exception {
+    System.out.println(input);
+    String[] strings = StringUtils.split(input, ';');
+    String query = strings[0];
+    String spatialNode = strings[1];
+    String rectangleStr = strings[2];
+
+    // String result = "";
+    // Session session = driver.session();
+    // StatementResult rs = session.run(query);
+    // while (rs.hasNext()) {
+    // result += rs.next();
+    // }
+    // System.out.println(result);
+
+    RisoTreeConnector risoTreeConnector = new RisoTreeConnector(dbserviceRisoTree, dataset, 1);
+    List<String> refinedQueries =
+        risoTreeConnector.getRefinedQueries(query, spatialNode, rectangleStr);
+    return convertToFrontQuery(refinedQueries);
+  }
+
+  public String convertToFrontQuery(List<String> queries) {
+    String res = "";
+    int index = 0;
+    for (String query : queries) {
+      if (index == 0) {
+        res += query;
+      } else {
+        res += ";" + query;
+      }
+      index++;
     }
-    System.out.println(result);
-    driver.close();
-    return result;
+    return res;
   }
 
   @GET
